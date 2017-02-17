e8c6b4d9da5668f41dc93a77492c35d03be6be20
DESKTOP-8RMKUQ8\11710 <1171067930@qq.com>
2017-02-17 15:04:50 +0800

    no message

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name manageMaterialsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the manageMaterialsApp
+ */
+angular.module('manageMaterialsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name manageMaterialsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the manageMaterialsApp
+ */
+angular.module('manageMaterialsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
