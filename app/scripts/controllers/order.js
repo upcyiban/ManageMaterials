c711bf123187964504a2fbb6fa64162d5cb224ca
zhangfuli <1171067930@qq.com>
2017-02-27 11:07:43 +0800

    听大佬的话

--- app/scripts/controllers/order.js
+++ app/scripts/controllers/order.js
@@ -14,47 +14,47 @@ angular.module('manageMaterialsApp')
     $scope.organization = $rootScope.orderOrganization ;
     $scope.description = $rootScope.orderDescription ;
 
-    //日历
-    $scope.startTime = new Date();
-    $scope.options = {
-      customClass: getDayClass,
-      minDate: new Date(),
-      showWeeks: true
-    };
-
-    $scope.setDate = function(year, month, day) {
-      $scope.endTime = new Date(year, month, day);
-    };
-    var tomorrow = new Date();
-    tomorrow.setDate(tomorrow.getDate() + 1);
-    var afterTomorrow = new Date(tomorrow);
-    afterTomorrow.setDate(tomorrow.getDate() + 1);
-    $scope.events = [
-      {
-        date: tomorrow,
-        status: 'full'
-      },
-      {
-        date: afterTomorrow,
-        status: 'partially'
-      }
-    ];
-    function getDayClass(data) {
-      var date = data.date,
-        mode = data.mode;
-      if (mode === 'day') {
-        var dayToCheck = new Date(date).setHours(0,0,0,0);
-
-        for (var i = 0; i < $scope.events.length; i++) {
-          var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
-
-          if (dayToCheck === currentDay) {
-            return $scope.events[i].status;
-          }
-        }
-      }
-      return '';
-    }
+    //日历  不让用日历
+    // $scope.startTime = new Date();
+    // $scope.options = {
+    //   customClass: getDayClass,
+    //   minDate: new Date(),
+    //   showWeeks: true
+    // };
+    //
+    // $scope.setDate = function(year, month, day) {
+    //   $scope.endTime = new Date(year, month, day);
+    // };
+    // var tomorrow = new Date();
+    // tomorrow.setDate(tomorrow.getDate() + 1);
+    // var afterTomorrow = new Date(tomorrow);
+    // afterTomorrow.setDate(tomorrow.getDate() + 1);
+    // $scope.events = [
+    //   {
+    //     date: tomorrow,
+    //     status: 'full'
+    //   },
+    //   {
+    //     date: afterTomorrow,
+    //     status: 'partially'
+    //   }
+    // ];
+    // function getDayClass(data) {
+    //   var date = data.date,
+    //     mode = data.mode;
+    //   if (mode === 'day') {
+    //     var dayToCheck = new Date(date).setHours(0,0,0,0);
+    //
+    //     for (var i = 0; i < $scope.events.length; i++) {
+    //       var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+    //
+    //       if (dayToCheck === currentDay) {
+    //         return $scope.events[i].status;
+    //       }
+    //     }
+    //   }
+    //   return '';
+    // }
 
     $scope.borrowerSubmit = function () {
       var phone = /^1[34578]\d{9}$/;
